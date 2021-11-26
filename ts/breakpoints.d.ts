0b110bcc598adc957fb7a8a3604dedc61b030826
rafalwlodarek <61891339+rafalwlodarek@users.noreply.github.com>
2021-11-26 14:09:58 +0100

    Initial commit
    Added breakpoints as ts variables

---
+++ ts/breakpoints.d.ts
@@ -0,0 +1,10 @@
+/**
+ * Do not edit directly
+ * Generated on Fri, 26 Nov 2021 13:08:12 GMT
+ */
+
+export const BreakpointXs : string;
+export const BreakpointS : string;
+export const BreakpointM : string;
+export const BreakpointL : string;
+export const BreakpointXl : string;
---
+++ ts/breakpoints.ts
@@ -0,0 +1,10 @@
+/**
+ * Do not edit directly
+ * Generated on Fri, 26 Nov 2021 13:08:12 GMT
+ */
+
+export const BreakpointXs = "(max-width: 639px)";
+export const BreakpointS = "(min-width: 640px) and (max-width: 999px";
+export const BreakpointM = "(min-width: 1000px) and (max-width: 1239px)";
+export const BreakpointL = "(min-width: 1240px) and (max-width: 1439px)";
+export const BreakpointXl = "(min-width: 1440px)";
